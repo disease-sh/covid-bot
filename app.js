5034cf74a6cba30507b4dcb088efadbcb8683854
Julian Pufler <puf17640@spengergasse.at>
2020-05-07 23:43:41 +0200

    commands added

--- app.js
+++ app.js
@@ -74,126 +74,13 @@ client.on('message', async message => {
 			case 'graph':
 			case 'history':
 				//#region 
-				const lineData = args.length > 2 ? await api.historical.countries({ country: args.splice(2).join(' ').trim(), days: -1 }) : {timeline: await api.historical.all({days: -1})}
-				buffer = await lineRenderer.renderToBuffer({
-					type: 'line',
-					data: {
-						labels: Object.keys(lineData.timeline.cases),
-						datasets: [{
-							label: "Cases",
-							borderColor: '#ffffff',
-							pointBackgroundColor: '#ffffff',
-							pointRadius: 2,
-							borderWidth: 3,
-							data: Object.keys(lineData.timeline.cases).map(key => lineData.timeline.cases[key])
-						},
-						{
-							label: "Deaths",
-							borderColor: '#E26363',
-							pointBackgroundColor: '#E26363',
-							pointRadius: 2,
-							borderWidth: 3,
-							data: Object.keys(lineData.timeline.deaths).map(key => lineData.timeline.deaths[key])
-						},
-						{
-							label: "Recovered",
-							borderColor: '#74D99F',
-							pointBackgroundColor: '#74D99F',
-							pointRadius: 2,
-							borderWidth: 3,
-							data: Object.keys(lineData.timeline.recovered).map(key => lineData.timeline.recovered[key])
-						},
-						{
-							label: "Active",
-							borderColor: '#FAE29F',
-							pointBackgroundColor: '#FAE29F',
-							pointRadius: 2,
-							borderWidth: 3,
-							data: Object.keys(lineData.timeline.cases).map(key => lineData.timeline.cases[key] - lineData.timeline.recovered[key] - lineData.timeline.deaths[key])
-						}]
-					},
-					options: {
-						scales: {
-							xAxes: [{
-								display: true,
-								ticks: {
-									fontSize: 17.5,
-									callback: (label) => moment(label, 'M/D/YY').format('DD MMM'),
-									padding: 10
-								},
-								gridLines: {
-									zeroLineColor: '#fff',
-									zeroLineWidth: 2
-								}
-							}],
-							yAxes: [{
-								display: true,
-								ticks: {
-									fontSize: 17.5,
-									callback: formatNumber 
-								},
-								gridLines: {
-									zeroLineColor: '#fff',
-									zeroLineWidth: 2
-								}
-							}]
-						},
-						legend: {
-							display: true,
-							labels: {
-								usePointStyle: true,
-								padding: 25,
-								fontSize: 15
-							}
-						}
-					}
-				})
-				embed = createEmbed({
-					color: '#303136', 
-					author: { name: 'COVID Stats by NovelCOVID', url: 'https://img.icons8.com/ios-filled/50/000000/virus.png' },
-					title: `${lineData.country || 'Global'} Timeline`,
-					description: 'Data is provided by John Hopkins University.',
-					files: [new Discord.MessageAttachment(buffer, 'graph.png')],
-					image: 'attachment://graph.png',
-					footer: 'Fetched from https://disease.sh'
-				})
-				await channel.send(embed)
+				
 				break
 				//#endregion
 			case 'o':
 			case 'overview':
 				//#region 
-				const pieData = args.length > 2 ? await api.countries({ country: args.splice(2).join(' ').trim() }) : await api.all()
-				buffer = await pieRenderer.renderToBuffer({
-					type: 'pie',
-					data: {
-						labels: ['Active', 'Recovered', 'Deaths'],
-						datasets: [{
-							data: [pieData.active, pieData.recovered, pieData.deaths],
-							backgroundColor: ['#FAE29F', '#7FD99F', '#E26363'],
-							borderWidth: 0.5,
-							borderColor: ['#FAE29F', '#7FD99F', '#E26363']
-						}]
-					},
-					options: {
-						legend: {
-							display: true,
-							labels: {
-								padding: 40,
-								fontSize: 30
-							}
-						}
-					}
-				})
-				embed = createEmbed({
-					color: '#303136', 
-					author: { name: 'COVID Stats by NovelCOVID', url: 'https://img.icons8.com/ios-filled/50/000000/virus.png' },
-					title: `${pieData.country || 'Global'} Overview`,
-					files: [new Discord.MessageAttachment(buffer, 'graph.png')],
-					image: 'attachment://graph.png',
-					footer: 'Fetched from https://disease.sh'
-				})
-				await channel.send(embed)
+				
 				break
 				//#endregion
 			case 'a':
@@ -230,31 +117,7 @@ client.on('message', async message => {
 			case 's':
 			case 'state':
 				//#region 
-				if (args.length < 3){
-					await message.reply('Please specify a state name.')
-					return
-				}
-				data = { state: args.splice(2).join(' ').trim() }
-				const stateData = await api.states(data)
-				const yesterdayStateData = await api.yesterday.states(data)
-				stateData.todayActives = stateData.active - yesterdayStateData.active
-				stateData.todayTests = stateData.tests - yesterdayStateData.tests
-				embed = createEmbed({
-					color: '#303136', 
-					author: { name: 'COVID Stats by NovelCOVID', url: 'https://img.icons8.com/ios-filled/50/000000/virus.png' },
-					thumbnail: 'https://disease.sh/assets/img/flags/us.png',
-					title: `${stateData.state}, USA`,
-					fields: [
-						{ name: 'Cases', value: `${formatNumber(stateData.cases)}\n(${(stateData.todayCases >= 0 ? "+":"-")+String(Math.abs(stateData.todayCases)).replace(/(.)(?=(\d{3})+$)/g,'$1,')})`, inline: true },
-						{ name: 'Deaths', value: `${formatNumber(stateData.deaths)}\n(${(stateData.todayDeaths >= 0 ? "+":"-")+String(Math.abs(stateData.todayDeaths)).replace(/(.)(?=(\d{3})+$)/g,'$1,')})`, inline: true },
-						{ name: 'Active', value: `${formatNumber(stateData.active)}\n(${(stateData.todayActives >= 0 ? "+":"-")+String(Math.abs(stateData.todayActives)).replace(/(.)(?=(\d{3})+$)/g,'$1,')})`, inline: true },
-						{ name: 'Tests', value: `${formatNumber(stateData.tests)}\n(${(stateData.todayTests >= 0 ? "+":"-")+String(Math.abs(stateData.todayTests)).replace(/(.)(?=(\d{3})+$)/g,'$1,')})`, inline: true },
-						{ name: 'Test rate', value: `${(stateData.testsPerOneMillion/10000).toFixed(4)} %`, inline: true },
-						{ name: 'Last Updated', value: moment(stateData.updated).fromNow(), inline: true }
-					],
-					footer: 'Fetched from https://disease.sh'
-				})
-				await channel.send(embed)
+				
 				break
 				//#endregion
 			case 'l':
@@ -262,17 +125,7 @@ client.on('message', async message => {
 			case 'rank':
 			case 'leaderboard':
 				//#region 
-				let max = typeof(parseInt(args[2])) !== 'number' ? 10 : parseInt(args[2])
-				const totalCases = (await api.all()).cases
-				const leaderboard = (await api.countries({ sort: 'cases' })).splice(0, max > 25 ? 25 : max)
-				embed = createEmbed({
-					color: '#303136', 
-					author: { name: 'COVID Stats by NovelCOVID', url: 'https://img.icons8.com/ios-filled/50/000000/virus.png' },
-					title: `Top ${max} Countries`,
-					description: leaderboard.map((c, index) => `**${++index}**. ${c.country} \u279C ${(c.cases/totalCases*100).toFixed(2)} %`).join('\n'),
-					footer: 'Fetched from https://disease.sh'
-				})
-				await channel.send(embed)
+				
 				break
 				//#endregion
 		}
