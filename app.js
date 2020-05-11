f70d4776105578ff92adef53b079bb2edd1f8271
Julian Pufler <puf17640@spengergasse.at>
2020-05-11 17:20:05 +0200

    fixxxx

--- app.js
+++ app.js
@@ -18,9 +18,8 @@ client.once('ready', () => {
 })
 
 client.on('message', async message => {
-	message.content = message.content.toLowerCase()
 	const parsed = parser.parse(message, prefix, { allowSpaceBeforeCommand: true })
-	if (!parsed.success && commands[parsed.command]) {
+	if (parsed.success && commands[parsed.command]) {
 		commands[parsed.command](message, parsed.arguments)
 	}
 })
