8e35f73885925add8d96133758e73dc7948dfcbc
Julian Pufler <puf17640@spengergasse.at>
2020-05-21 23:32:59 +0200

    memory improvements i guess

--- app.js
+++ app.js
@@ -2,7 +2,52 @@ const Discord = require('discord.js'),
 	parser = require('discord-command-parser'),
 	commands = require('./commands')
 
-const client = new Discord.Client(),
+const client = new Discord.Client({
+	presence: {
+		status: "online",
+		activity: {
+			type: 'WATCHING', 
+			name: 'NovelCOVID', 
+			url: 'https://disease.sh'
+		}
+	},
+	restTimeOffset: 100,
+	partials: ["MESSAGE", "CHANNEL", "REACTION"],
+	ws: {
+		Intents: ["MESSAGE_CREATE", "MESSAGE_UPDATE", "GUILD_MESSAGE_REACTIONS"],
+	},
+	disabledEvents: [
+		"GUILD_MEMBER_ADD",
+		"GUILD_MEMBER_REMOVE",
+		"GUILD_MEMBER_UPDATE",
+		"GUILD_MEMBERS_CHUNK",
+		"GUILD_INTEGRATIONS_UPDATE",
+		"GUILD_ROLE_CREATE",
+		"GUILD_ROLE_DELETE",
+		"GUILD_ROLE_UPDATE",
+		"GUILD_BAN_ADD",
+		"GUILD_BAN_REMOVE",
+		"GUILD_EMOJIS_UPDATE",
+		"CHANNEL_PINS_UPDATE",
+		"CHANNEL_CREATE",
+		"CHANNEL_DELETE",
+		"CHANNEL_UPDATE",
+		"MESSAGE_DELETE",
+		"MESSAGE_DELETE_BULK",
+		"MESSAGE_REACTION_REMOVE",
+		"MESSAGE_REACTION_REMOVE_ALL",
+		"MESSAGE_REACTION_REMOVE_EMOJI",
+		"USER_UPDATE",
+		"USER_SETTINGS_UPDATE",
+		"PRESENCE_UPDATE",
+		"TYPING_START",
+		"VOICE_STATE_UPDATE",
+		"VOICE_SERVER_UPDATE",
+		"INVITE_CREATE",
+		"INVITE_DELETE",
+		"WEBHOOKS_UPDATE",
+	]
+}),
 	config = require('dotenv').config()
 
 if (config.error) {
@@ -14,7 +59,6 @@ const prefix = process.env.PREFIX || 'cov'
 
 client.once('ready', () => {
 	console.log('[INFO]: bot is running')
-	client.user.setActivity({ status: 'online', type: 3, name: 'NovelCOVID', url: 'https://disease.sh' })
 })
 
 client.on('message', async message => {
